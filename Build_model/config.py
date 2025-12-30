2ea8614fe9343bda3b0b5c9844d7a344f41d9755
Your Name <you@example.com>
2025-12-30 17:38:51 +0700

    Khởi tạo dự án, thêm biến toàn cục tại file config.py

---
+++ Build_model/config.py
@@ -0,0 +1,28 @@
+# src/config.py
+import os
+
+VOCAB_DIR = "./Training_Data/vocab"
+VOCAB_EN_FILE = VOCAB_DIR+"vocab_en.pkl"
+VOCAB_VI_FILE = VOCAB_DIR+"vocab_vi.pkl"
+
+# 1. Đường dẫn dữ liệu
+RAW_DIR = "./Training_Data/raw"
+PROCESSED_DIR = "./Training_Data/processed"
+
+# 2 file dataset đã giải nén
+RAW_EN_FILE = RAW_DIR+"/OpenSubtitles.en-vi.en"
+RAW_VI_FILE = RAW_DIR+"/OpenSubtitles.en-vi.vi"
+
+# 3. File sau khi làm sạch
+CLEAN_EN_FILE = PROCESSED_DIR+"/train.en"
+CLEAN_VI_FILE = PROCESSED_DIR+"/train.vi"
+
+# 4. Hyperparameters cho Model & Data
+VOCAB_SIZE = 20000
+MAX_LENGTH = 60
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# 5. Token
+START_TOKEN = "[start]"
+END_TOKEN = "[end]"
